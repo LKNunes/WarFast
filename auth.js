ed09d63e3e4458a871c8c2cb705fe0f3b4c9c0a7
LKNunes <7010102+LKNunes@users.noreply.github.com>
2024-06-07 12:03:20 -0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -3,7 +3,7 @@ async function login() {
     const senha = document.getElementById('senhaLogin').value;
 
     try {
-        const response = await fetch('https://dbwar.onrender.com', {
+        const response = await fetch('https://dbwar.onrender.com/usuarios', {
             method: 'GET',
         });
 
@@ -52,7 +52,7 @@ async function cadastrarUsuario() {
     const senhaUsuario = document.getElementById('senhaUsuario').value;
 
     try {
-        const response = await fetch('https://dbwar.onrender.com/', {
+        const response = await fetch('https://dbwar.onrender.com/usuarios', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
