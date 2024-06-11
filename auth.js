c5fca0a081da1b1ef039534a890f7da71036a3c0
LKNunes <7010102+LKNunes@users.noreply.github.com>
2024-06-11 13:02:44 -0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -106,7 +106,7 @@ async function signup() {
         const updateResponse = await fetch('https://api.github.com/repos/LKNunes/WarFast/contents/DB/db.json', {
             method: 'PUT',
             headers: {
-                'Authorization': 'Bearer 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',
+                'Authorization': 'Bearer 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',
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
